b9f7aa983f9f9a4c62da2239aee1f04275ead2c3
Lewis <63010421+LewisScript@users.noreply.github.com>
2021-02-17 07:34:27 -0800

    Introduction to variables and data types
    Introduction to variables and different data types such as,
    
    Strings
    Ints
    Floats
    Booleans

---
+++ python-content/Beginner/Variables_And_Data_Types.py
@@ -0,0 +1,32 @@
+# Strings
+
+string = "This is a string!"
+
+print(string)
+
+# Ints
+
+a = 1
+b = 2
+
+print(a)
+
+print(b)
+
+print(a + b)
+
+# Floats
+
+a = 124.32
+b = 532.45
+
+print(a + b)
+
+# Booleans (Bools)
+
+a = True
+b = False
+
+print(a)
+
+print(b)
