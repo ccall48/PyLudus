40131d11309d94f8107acb8e02047e8bcb4d835a
Lewis <63010421+LewisScript@users.noreply.github.com>
2021-02-17 07:16:25 -0800

    Examples Of Basic Math Calculations
    This .py file contains examples of addition, subtraction, multiplication and division all with python, theres many more calculations but this is just the basics for now.

---
+++ python-content/Beginner/Simple_Math_Calculations.py
@@ -0,0 +1,31 @@
+# Addition
+
+print(5 + 5)
+
+print(10 + 10)
+
+print(1230434495 + 4556675765)
+
+# Subtraction
+
+print(5 - 5)
+
+print(10 - 20)
+
+print(134454345 - 34344532)
+
+# Multiplication
+
+print(62 * 62)
+
+print(100 * 10)
+
+print(34656 * 234)
+
+# Division
+
+print(10 / 10)
+
+print(18 / 430)
+
+print(100002 / 888324) 
